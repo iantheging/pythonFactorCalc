e22368b15bf01179932f1912aba76bef7b5dfbce
Ian Seal <iantheging@gmail.com>
2018-08-10 12:01:50 -0500

    Apparently there was change

--- ImportTest.py
+++ ImportTest.py
@@ -1,4 +1,4 @@
-import classes as c
+from classes import *
 
 while True:
     print("\nRectangle Program: Please input points in a clockwise fashion in x,y form")
@@ -6,11 +6,11 @@ while True:
     p2x, p2y = input("Point 2: ").split(',')
     p3x, p3y = input("Point 3: ").split(',')
     p4x, p4y = input("Point 4: ").split(',')
-    p1 = c.Point(float(p1x), float(p1y))
-    p2 = c.Point(float(p2x), float(p2y))
-    p3 = c.Point(float(p3x), float(p3y))
-    p4 = c.Point(float(p4x), float(p4y))
-    r = c.Rectangle(p1, p2, p3, p4)
+    p1 = Point(float(p1x), float(p1y))
+    p2 = Point(float(p2x), float(p2y))
+    p3 = Point(float(p3x), float(p3y))
+    p4 = Point(float(p4x), float(p4y))
+    r = Rectangle(p1, p2, p3, p4)
     userChoice = input("Enter p for perimeter of a for area: ")
     if userChoice is 'p':
         print(f'The perimeter is {r.perimeter()}')
