6abb197c84b07bac984f6adfa859d11bfaae39dd
Ian Seal <iantheging@gmail.com>
2018-08-10 10:56:00 -0500

    Learned how import works

---
+++ ImportTest.py
@@ -0,0 +1,22 @@
+import classes as c
+
+while True:
+    print("\nRectangle Program: Please input points in a clockwise fashion in x,y form")
+    p1x, p1y = input("Point 1: ").split(',')
+    p2x, p2y = input("Point 2: ").split(',')
+    p3x, p3y = input("Point 3: ").split(',')
+    p4x, p4y = input("Point 4: ").split(',')
+    p1 = c.Point(float(p1x), float(p1y))
+    p2 = c.Point(float(p2x), float(p2y))
+    p3 = c.Point(float(p3x), float(p3y))
+    p4 = c.Point(float(p4x), float(p4y))
+    r = c.Rectangle(p1, p2, p3, p4)
+    userChoice = input("Enter p for perimeter of a for area: ")
+    if userChoice is 'p':
+        print(f'The perimeter is {r.perimeter()}')
+    else:
+        print(f"The area is {r.area()}")
+    userEnd = input("Enter q to quit program: ")
+    if userEnd is 'q':
+        print("Goodbye")
+        break
--- classes.py
+++ classes.py
@@ -71,23 +71,3 @@ print('{0:.4}'.format(t.perimeter()))
 # t2 = Triangle(pa, pb, pc)
 #
 # print('{0:.4}'.format(t2.perimeter()))
-while True:
-    print("\nRectangle Program: Please input points in a clockwise fashion in x,y form")
-    p1x, p1y = input("Point 1: ").split(',')
-    p2x, p2y = input("Point 2: ").split(',')
-    p3x, p3y = input("Point 3: ").split(',')
-    p4x, p4y = input("Point 4: ").split(',')
-    p1 = Point(float(p1x), float(p1y))
-    p2 = Point(float(p2x), float(p2y))
-    p3 = Point(float(p3x), float(p3y))
-    p4 = Point(float(p4x), float(p4y))
-    r = Rectangle(p1, p2, p3, p4)
-    userChoice = input("Enter p for perimeter of a for area: ")
-    if userChoice is 'p':
-        print(f'The perimeter is {r.perimeter()}')
-    else:
-        print(f"The area is {r.area()}")
-    userEnd = input("Enter q to quit program: ")
-    if userEnd is 'q':
-        print("Goodbye")
-        break
